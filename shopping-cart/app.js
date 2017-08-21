f672ff273186861f2adaf59e83ba4e3a8531f692
utkarsh_bajpai <ubajpai1aa@gmail.com>
2017-08-21 21:22:46 +0530

    some errors in the sessions module call
    some errors in the sessions module call

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -30,7 +30,7 @@ app.use(logger('dev')); // app.use is basically a middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({secret: 'mysupersecret', resave: false, saveUninitialised: false}));
+app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
