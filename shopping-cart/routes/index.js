38ec1174954439d9ed882635f2ecf2c62ce2eeb6
utkarsh_bajpai <ubajpai1aa@gmail.com>
2017-08-27 22:31:24 +0530

    flash validation using passport authentication
    flash validation using passport authentication

--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -24,8 +24,11 @@ router.get('/', function(req, res, next) {
   
 });
 
+
+
 router.get('/user/signup', function(req, res, next){
-	res.render('users/signup', {csrfToken : req.csrfToken()});
+  var messages = req.flash('error');
+	res.render('users/signup', {csrfToken : req.csrfToken(), messages : messages, hasErrors : messages.length > 0});
 });
 
 router.post('/user/signup', passport.authenticate('local.signup', {
