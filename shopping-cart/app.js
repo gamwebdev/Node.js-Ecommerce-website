027c0931daab4a5dc08eab04d487329c072165b1
utkarsh_bajpai <ubajpai1aa@gmail.com>
2017-08-29 01:20:49 +0530

    setting up of express-validators
    setting up of express-validators

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -10,6 +10,7 @@ mongoose.Promise      = global.Promise;
 var session           = require('express-session');
 var passport          = require('passport');
 var flash             = require('connect-flash');
+var validator         = require('express-validator');
 var index             = require('./routes/index');
 
 var app               = express();
@@ -32,6 +33,7 @@ app.set('view engine', 'hbs');
 app.use(logger('dev')); // app.use is basically a middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(flash());
