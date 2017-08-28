ac3dcd39e149f345b56bbe270363f8627a856851
utkarsh_bajpai <ubajpai1aa@gmail.com>
2017-08-29 01:36:37 +0530

    validation using express-validator npm package
    validation using express-validator npm package

--- shopping-cart/config/passport.js
+++ shopping-cart/config/passport.js
@@ -17,6 +17,17 @@ passport.use('local.signup', new LocalStrategy({
 	passwordField		: 'password',
 	passReqToCallback 	: true
 }, function(req, email, password, done){
+	req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+	req.checkBody('password', 'Invalid password').notEmpty().isLength({min:4});
+	var errors = req.validationErrors(); // array
+	if(errors){
+		var messages = [];
+		errors.forEach(function(error){
+			messages.push(error.msg);
+		});
+		return done(null, false, req.flash('error', messages)); // false means req was not successfull because of validation and null means no technical error
+	}
+
 	User.findOne({'email' : email}, function(err, user){
 		if(err){
 			return done(err);
