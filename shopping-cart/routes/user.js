c6689bc15d9207f2b0d88720411257d0995ab78e
utkarsh_bajpai <ubajpai1aa@gmail.com>
2017-09-06 12:26:13 +0530

    login , signup and logout logic
    login , signup and logout logic

--- shopping-cart/routes/user.js
+++ shopping-cart/routes/user.js
@@ -6,6 +6,17 @@ var passport 		= require('passport');
 var csrfProtection  = csrf();
 router.use(csrfProtection);
 
+
+router.get('/profile', isLoggedIn, function(req, res, next){
+	res.render('users/profile');
+});
+
+
+
+router.use('/', notLoggedIn, function(req, res, next){
+	next();
+});
+
 router.get('/signup', function(req, res, next){
   var messages = req.flash('error');
 	res.render('users/signup', {csrfToken : req.csrfToken(), messages : messages, hasErrors : messages.length > 0});
@@ -28,23 +39,29 @@ router.post('/signin', passport.authenticate('local.signin', {
   failureFlash  : true 
 }));
 
-router.get('/profile', isLoggedIn, function(req, res, next){
-	res.render('users/profile');
-});
 
-router.get('logout', function(req, res, next){
+router.get('logout', function(req, res, next){ // logout functionality not working properly
 	req.logout();   		// passport method
 	res.redirect('/');
 });
+
+
 
 
 function isLoggedIn(req, res, next){
 	if(req.isAuthenticated()){ // method of passport
 		return next();
 	}
-	res.redirect('/');
+	res.redirect('/user/signin');
 }
 
+function notLoggedIn(req, res, next){
+	if(!req.isAuthenticated()){
+		return next();
+	}
+
+	res.redirect('/');
+}
 
 
 module.exports = router;
