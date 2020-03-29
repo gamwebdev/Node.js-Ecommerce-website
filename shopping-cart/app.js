8ab0c9239463eeabc4deeadbf4081e54ecd9dc89
Utkarsh Bajpai <ubajpai1aa@gmail.com>
2020-03-29 17:16:47 +0530

    Some changes

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,54 +1,56 @@
-var express           = require('express');
-var path              = require('path');
-var favicon           = require('serve-favicon');
-var logger            = require('morgan');
-var cookieParser      = require('cookie-parser');
-var bodyParser        = require('body-parser');
-var expressHbs        = require('express-handlebars');
-const mongoose 	      = require('mongoose');
-mongoose.Promise      = global.Promise;
-var session           = require('express-session');
-var passport          = require('passport');
-var flash             = require('connect-flash');
-var validator         = require('express-validator');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressHbs = require('express-handlebars');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
-var userRoutes        = require('./routes/user');
-var index             = require('./routes/index');
+var routes = require('./routes/index');
+var userRoutes = require('./routes/user');
 
-var app               = express();
+var app = express();
 
-mongoose.connect('mongodb://localhost/shopping', { useMongoClient: true });
-mongoose.connection.once('open', function(){
-    console.log('connection has been made successfully');
-  }).on('error', function(error){
-    console.log('connection has been terminated');
-});
-
-var configPassport    = require('./config/passport');
+mongoose.connect('localhost:27017/shopping');
+require('./config/passport');
 
 // view engine setup
-app.engine('.hbs', expressHbs({defaultLayout: 'layouts', extname: '.hbs'}));
-app.set('view engine', 'hbs');
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev')); // app.use is basically a middleware
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(session({
+  secret: 'mysupersecret', 
+  resave: false, 
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 180 * 60 * 1000 }
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(function(req, res, next){
-  res.locals.login = req.isAuthenticated();
+
+app.use(function(req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    res.locals.session = req.session;
+    next();
 });
 
 app.use('/user', userRoutes);
-app.use('/', index);
-
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -57,15 +59,29 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
--- shopping-cart/routes/user.js
+++ shopping-cart/routes/user.js
@@ -1,67 +1,85 @@
-var express 		= require('express');
-var router 			= express.Router();
-var csrf 			= require('csurf');
-var passport 		= require('passport');
+var express = require('express');
+var router = express.Router();
+var csrf = require('csurf');
+var passport = require('passport');
 
-var csrfProtection  = csrf();
+var Order = require('../models/order');
+var Cart = require('../models/cart');
+
+var csrfProtection = csrf();
 router.use(csrfProtection);
 
-
-router.get('/profile', isLoggedIn, function(req, res, next){
-	res.render('users/profile');
+router.get('/profile', isLoggedIn, function (req, res, next) {
+    Order.find({user: req.user}, function(err, orders) {
+        if (err) {
+            return res.write('Error!');
+        }
+        var cart;
+        orders.forEach(function(order) {
+            cart = new Cart(order.cart);
+            order.items = cart.generateArray();
+        });
+        res.render('user/profile', { orders: orders });
+    });
 });
 
-
-
-router.use('/', notLoggedIn, function(req, res, next){
-	next();
+router.get('/logout', isLoggedIn, function (req, res, next) {
+    req.logout();
+    res.redirect('/');
 });
 
-router.get('/signup', function(req, res, next){
-  var messages = req.flash('error');
-	res.render('users/signup', {csrfToken : req.csrfToken(), messages : messages, hasErrors : messages.length > 0});
+router.use('/', notLoggedIn, function (req, res, next) {
+    next();
+});
+
+router.get('/signup', function (req, res, next) {
+    var messages = req.flash('error');
+    res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
 });
 
 router.post('/signup', passport.authenticate('local.signup', {
-	successRedirect : '/user/profile', 
-	failureRedirect : '/user/signup',
-	failureFlash	: true
-}));
+    failureRedirect: '/user/signup',
+    failureFlash: true
+}), function (req, res, next) {
+    if (req.session.oldUrl) {
+        var oldUrl = req.session.oldUrl;
+        req.session.oldUrl = null;
+        res.redirect(oldUrl);
+    } else {
+        res.redirect('/user/profile');
+    }
+});
 
-router.get('/signin', function(req, res, next){
+router.get('/signin', function (req, res, next) {
     var messages = req.flash('error');
-    res.render('users/signin', {csrfToken : req.csrfToken(), messages : messages, hasErrors : messages.length > 0});
+    res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
 });
 
 router.post('/signin', passport.authenticate('local.signin', {
-  successRedirect : '/user/profile', 
-  failureRedirect : '/user/signin',
-  failureFlash  : true 
-}));
-
-
-router.get('/logout', function(req, res, next){ // logout functionality not working properly
-	req.logout();   		// passport method
-	res.redirect('/');
+    failureRedirect: '/user/signin',
+    failureFlash: true
+}), function (req, res, next) {
+    if (req.session.oldUrl) {
+        var oldUrl = req.session.oldUrl;
+        req.session.oldUrl = null;
+        res.redirect(oldUrl);
+    } else {
+        res.redirect('/user/profile');
+    }
 });
 
-
-
-
-function isLoggedIn(req, res, next){
-	if(req.isAuthenticated()){ // method of passport
-		return next();
-	}
-	res.redirect('/user/signin');
-}
-
-function notLoggedIn(req, res, next){
-	if(!req.isAuthenticated()){
-		return next();
-	}
-
-	res.redirect('/');
-}
-
-
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
+
+function notLoggedIn(req, res, next) {
+    if (!req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
